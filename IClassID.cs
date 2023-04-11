135005be8f742c97a1be06c5db5c3d54a8a102d3
JaBober <55249165+Oleg0910@users.noreply.github.com>
2023-04-12 02:55:29 +0300

    Add good code

---
+++ IClassID.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace o_2_C
+{
+    internal interface IClassGenerics<T>
+    {
+        int ID { get; }
+        string ToCSVString();
+
+        static T UserInput()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
