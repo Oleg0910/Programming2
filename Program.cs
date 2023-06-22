9eb23cd33dd643529da6f2bd5414037ae1b834b7
JaBober <55249165+Oleg0910@users.noreply.github.com>
2023-06-22 23:11:01 +0300

    Додав роботу

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace talon_summer_2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Anagram.AnagramMenu();
+            MergeSort.MergeSortMenu();
+            ThirdTask.ThirdMenu();
+        }
+    }
+}
---
+++ Validation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace talon_summer_2023
+{
+    public class Validation
+    {
+        public static string ValidateWord(string word)
+        {
+            foreach (char c in word)
+            {
+                if (!char.IsLetter(c)) throw new ArgumentException($"!!!!{word} contains not only letters!!!!");
+            }
+
+            return word;
+        }
+
+        public static int ValidateSize(int num)
+        {
+            if (num < 0) throw new ArgumentException("size can not be negative");
+
+            return num;
+        }
+
+
+    }
+}
